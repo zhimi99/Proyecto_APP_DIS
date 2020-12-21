dd96160c19d10e5d506c29b71fca8e0f1aa328da
zhimi99 <52504869+zhimi99@users.noreply.github.com>
2020-12-20 23:25:51 -0500

    Empieza Creacion clases Beans

--- src/main/java/app/proyecto/SistemaBancario/negocio/CuentaON.java
+++ src/main/java/app/proyecto/SistemaBancario/negocio/CuentaON.java
@@ -3,14 +3,14 @@ package app.proyecto.SistemaBancario.negocio;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import app.proyecto.SistemaBancario.DAO.CuentaDAO;
+import app.proyecto.SistemaBancario.DAO.cuentaDAO;
 import app.proyecto.SistemaBancario.Entidades.Cuenta;
 
 @Stateless
 public class CuentaON {
 
 	@Inject
-	CuentaDAO cuentadao;
+	cuentaDAO cuentadao;
 	
 	public void crearCuenta(Cuenta cuenta) {
 		this.cuentadao.crearCuenta(cuenta);
--- src/main/java/app/proyecto/SistemaBancario/negocio/UsuarioON.java
+++ src/main/java/app/proyecto/SistemaBancario/negocio/UsuarioON.java
@@ -3,13 +3,13 @@ package app.proyecto.SistemaBancario.negocio;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import app.proyecto.SistemaBancario.DAO.UsuarioDAO;
+import app.proyecto.SistemaBancario.DAO.usuarioDAO;
 import app.proyecto.SistemaBancario.Entidades.Usuario;
 
 @Stateless
 public class UsuarioON {
 	@Inject
-	UsuarioDAO usuariodao;
+	usuarioDAO usuariodao;
 	
 	public void crearUsuario(Usuario usuario) {
 		System.out.println("en on "+ usuario.toString());
